99fca29747881811f0016569bfdbdf20ceffb09c
noijd <ju.an.marluc@gmail.com>
2015-08-01 00:23:41 +0100

    Implement the Evidence class

--- lib/eurocrat/evidence.rb
+++ lib/eurocrat/evidence.rb
@@ -1,4 +1,8 @@
 module Eurocrat
+  #
+  #
+  # It normalizes all the country codes to ISO 3166-1 alpha-2
+  #
   class Evidence
 
     # TODO extensible
@@ -13,30 +17,35 @@ module Eurocrat
     class << self
 
       def from_country_code code
-        new code
-      end
-
-      def from_geocoder_result result
-        new result.country_code, result
-      end
-
-      def from_vat_number vat_number
-        new vat_number.country_code, vat_number
+        new code, code
       end
 
       # Tries to recognize an acceptable country code
       def from_hash hash
+        COUNTRY_CODE_KEYS.each do |key|
+          country_code = hash[key] || hash[key.to_sym]
+          return new(country_code, hash) if country_code
+        end
+
+        # TODO class
+        raise StandardError
       end
 
       # Tries to recognize an acceptable country code
       def from_object object
+        COUNTRY_CODE_KEYS.each do |key|
+          if object.respond_to? key.to_sym
+            return new object.__send__(key), object
+          end
+        end
+
+        # TODO class
+        raise StandardError
       end
 
-      def new source
+      def from source
         builder = case source
           when Integer, String then :from_country_code
-          when Geocoder::Result then :from_geocoder_result
-          when VatNumber then :from_vat_number
           when Hash then :from_hash
           else :from_object
         end
@@ -46,13 +55,15 @@ module Eurocrat
 
     end
 
-    def initialize country_code, data=nil
-    end
-
     # ISO 3166-1 alpha-2
     attr_reader :country_code
 
+    # Object that contains the country code that supports the evidence
     attr_reader :data
+
+    def initialize country_code, data
+      @country_code, @data = Country.to_alpha2(country_code), data
+    end
 
   end
 end
