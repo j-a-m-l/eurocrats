1e01374cabca435817c7ee7f23e7c1e87f56c02b
noijd <ju.an.marluc@gmail.com>
2015-08-02 20:55:25 +0100

    Implement the Context class

--- lib/eurocrat/context.rb
+++ lib/eurocrat/context.rb
@@ -1,3 +1,6 @@
+require_relative 'supplier'
+require_relative 'customer'
+
 require 'forwardable'
 
 module Eurocrat
@@ -9,7 +12,7 @@ module Eurocrat
   # * What are the VAT rates that correspond to that location?
   # * How much should be charged?
   #
-  # TODO This is the unique method that could be called before having obtained enough
+  # FIXME This is the unique method that could be called before having obtained enough
   # evidences of the customer location.
   # Other methods raise a `ConflictingEvidencesError` in that case because they
   # can't operate without knowing the country.
@@ -17,79 +20,151 @@ module Eurocrat
   class Context
     extend Forwardable
 
+    # TODO configurable in Eurocrat
+    VIES_EVIDENCE_LABEL = 'eurocrat.vies.validation'
+
     attr_reader :supplier
-
     attr_reader :customer
-    def_delegators :@customer, :evidences, :<<
-
-    attr_reader :judge
-    def_delegator :@customer, :conflicts
 
     # TODO options...
-    def initialize supplier=nil, customer=nil, judge=nil
+    def initialize supplier=nil, customer=nil
+      @evidences = {}
+
       # TODO default supplier
-      # @supplier ||= Supplier.new
-
-      @customer ||= Customer.new
-
-      # TODO default judge
-      @judge ||= Judge.new
+      @supplier ||= supplier ||= Eurocrat::Supplier.new
+      @customer ||= customer ||= Eurocrat::Customer.new
 
       # TODO default rate
       @rate = 'standard'
+
+      # TODO
+      @minimum = 2
     end
 
-    # def use_as_default_rate rate
-    #   @supplier.default_rate = rate
-    # end
+    def evidences
+      @evidences.merge(customer.collect_eurocrat_evidences)
+    end
+
+    def [] label
+      evidences[label]
+    end
+
+    def []= label, source
+      @evidences[label] = source.is_a?(Evidence) ? source : Evidence.from(source)
+    end
+
+    def only_location_evidences
+      evidences.reject {|e| e == VIES_EVIDENCE_LABEL }
+    end
+
+    # This method checks only the customer location evidences
+    #
+    # If you need a custom strategy for deciding about them, like prioritizing
+    # some evidences or ignoring others, you should be able to achieve that just
+    # overriding this method in an inherited Context class
+    #
+    # TODO example with billing_address, ip and credit card
+    # TODO example with billing_address, ip, credit card and phone
+    #
+    def non_conflicting_location_evidences
+      return only_location_evidences if only_location_evidences.size == 1
+
+      # by_country: { country_code => [[evidence_label, evidence_object], ...] }
+      by_country = only_location_evidences.group_by {|l, e| e.country_code }
+
+      # Gets the first country with at least 2 non conflicting evidences
+      non = by_country.find {|c, es_a| es_a.size >= @minimum }
+
+      # Converts to Hash again: { evidence_label_1 => evidence_object_1, ... }
+      (non && non[1] or {}).reduce({}) {|all, e_a| all.merge(e_a[0] => e_a[1]) }
+    end
+    alias favorable_evidences non_conflicting_location_evidences
+
+    # This method depends on the result of `non_conflicting_location_evidences`, so it should
+    # not be necessary to override it
+    def conflicting_location_evidences
+    end
+    alias conflicts conflicting_location_evidences
 
     def enough_evidences?
-      (@decided_country = @judge.verdict @customer) != false
+      unless evidences.empty?
+        return (b2b? && valid_vat_numbers? or favorable_evidences.size >= @minimum)
+      end
+
+      false
     end
     alias enough? enough_evidences?
 
-    def decided_country
-      raise ConflictingEvidencesError unless enough_evidences?
+    def evidenced_country_code
+      unless non_conflicting_location_evidences.size >= @minimum
+        raise ConflictingEvidencesError.new 'Operation requires enough non conflicting evidences'
+      end
 
-      @decided_country
+      non_conflicting_location_evidences.values.first.country_code
     end
 
-    def vat_rates
-      VatRates.for decided_country
+
+    def taxables?
+      @supplier.taxable? && @customer.taxable?
+    end
+    alias b2b? taxables?
+
+    # Stores automatically the VIES validation as an evidence
+    def validate_on_vies!
+      @evidences[VIES_EVIDENCE_LABEL] = Vies::Validation.new(@supplier, @customer).request!
+    end
+
+    # TODO If supplier or customer change their VAT this should be updated
+    def valid_vat_numbers?
+      return evidences[VIES_EVIDENCE_LABEL].valid? if evidences.has_key? VIES_EVIDENCE_LABEL
+      nil
     end
 
     # If customer is in the same country than supplier, it should be charged
     # Then, the supplier must pay that collected VAT to her tax authorities,
     # although she could deduct her paid VAT
-    def vat_must_be_charged?
-      if decided_country == @supplier.country
+    # => true
+    # => false
+    # => nil
+    def should_vat_be_charged?
+      if b2b? && valid_vat_numbers?
+         different_countries?
+      elsif Country.in_european_union? evidenced_country_code
         true
       else
-        not @customer.valid_vat_number?
+        false
+      end
+    end
+
+
+    def vat_rates
+      VatRates.for decided_country_code
+    end
+
+    # TODO add the "as" method to Numeric objects
+    def vat_for amount, rate=nil
+      if should_vat_be_charged?
+        rate ||= @rate
+        amount * vat_rates[rate] / 100
+      else
+        0
       end
     end
 
     # TODO add the "as" method to Numeric objects
-    # def vat_for amount, rate=nil
-    #   if vat_must_be_charged?
-    #     rate ||= @rate
-    #     amount * vat_rates[rate] / 100
-    #   else
-    #     0
-    #   end
-    # end
-
-    # def with_vat amount, rate=nil
-    #   amount * vat_for amount, rate
-    # end
-
-    # `conflicts` is forwarded to Judge
-    alias conflicting_evidences conflicts 
-
-    def non_conflicting_evidences
-      #evidences - conflicts
+    def add_vat_to amount, rate=nil
+      amount * vat_for(amount, rate)
     end
-    alias not_conflicting_evidences non_conflicting_evidences
+
+    def open &block
+      instance_eval &block
+    end
+
+    private
+
+      def different_countries?
+        @supplier.vat_number.country_code != @consumer.vat_number.country_code
+      end
 
   end
 
--- spec/lib/eurocrat/context_spec.rb
+++ spec/lib/eurocrat/context_spec.rb
@@ -1,83 +1,179 @@
 describe Eurocrat::Context do
 
+  let(:conflicting) {
+    {
+      'one' => Eurocrat::Evidence.from('AT'),
+      'two' => Eurocrat::Evidence.from('MT'),
+    }
+  }
+
+  let(:non_conflicting) {
+    {
+      'non_one' => Eurocrat::Evidence.from('CY'),
+      'non_two' => Eurocrat::Evidence.from('CY'),
+    }
+  }
+
+  let(:stub_evidences!) {
+    expect(subject).to receive(:evidences).and_return(evidences).at_least(:once)
+  }
+
   describe 'initialize' do
   end
 
-  describe '#<<' do
-    it 'returns itself' do
-      expect(subject << {}).to be subject
+  describe '#evidences' do
+    it 'collects the customer evidences'
+  end
+
+  describe '#[]' do
+  end
+
+  describe '#[]=' do
+    let(:label) { 'declared.country_code' }
+
+    context 'receiving an Evidence' do
+      let(:source) { Eurocrat::Evidence.from 'IT' }
+
+      it 'adds it as evidence' do
+        subject[label] = source
+        expect(subject.evidences[label]).to be source
+      end
     end
 
-    describe 'receiving a Hash' do
-      let(:evidences) {
-        { country: '' }
+    context 'receiving other Object' do
+      let(:source) { 'DE' }
+
+      it 'instantiates a new Evidence using that object as source and adds it as evidence' do
+        subject[label] = source
+        expect(subject.evidences[label]).to be_a Eurocrat::Evidence
+        expect(subject.evidences[label].source).to eq source
+      end
+    end
+  end
+
+  # TODO alias
+  describe '#favorable_evidences' do
+    context 'having no evidence' do
+      let(:evidences) { {} }
+
+      it 'returns an empty Hash' do
+        stub_evidences!
+        expect(subject.favorable_evidences).to eq({})
+      end
+    end
+
+    context 'having only 1 evidence' do
+      let(:evidences) { { 'one' => Eurocrat::Evidence.from('FR') } }
+
+      it 'returns its Hash' do
+        stub_evidences!
+        expect(subject.favorable_evidences).to eq evidences
+      end
+    end
+
+    context 'having 2 non conflicting evidences' do
+      let(:evidences) { non_conflicting }
+
+      it 'returns its Hash' do
+        stub_evidences!
+        expect(subject.favorable_evidences).to eq non_conflicting
+      end
+    end
+
+    context 'having 2 conflicting evidences' do
+      let(:evidences) { conflicting }
+
+      it 'returns an empty Hash' do
+        stub_evidences!
+        expect(subject.favorable_evidences).to eq({})
+      end
+    end
+
+    context 'having 2 non conflicting and 2 conflicting evidences' do
+      let(:evidences) { non_conflicting.merge conflicting }
+
+      it 'returns its Hash' do
+        stub_evidences!
+        expect(subject.favorable_evidences).to eq non_conflicting
+      end
+    end
+
+    # TODO more than 2
+    # TODO 2 + 2 different non_conflicting
+  end
+
+  # TODO alias
+  describe '#conflicting_evidences' do
+  end
+
+  # TODO alias
+  describe '#enough_evidences?' do
+    context 'b2b' do
+      let(:mock_b2b!) {
       }
 
-      describe 'for each entry' do
-        it 'checks that the value is an Alpha2 country code or raises an error'
-        it 'checks that the value is an Alpha3 country code or raises an error'
-        it 'checks that the value is an ISO country code or raises an error'
+      context 'valid VAT numbers' do
+        it 'returns true'
+      end
 
-        describe 'adds a new evidence' do
-          it 'with key as the evidence name' do
-            subject.evidences
-          end
-
-          it 'with its country, as Alpha2 code, as the evidence value'
-        end
+      context 'without valid VAT numbers' do
+        it 'checks the location evidences'
       end
     end
 
-    describe 'receiving a VatNumber object' do
-      it 'changes the customer VAT number'
+    context 'with non conflicting evidences' do
+      let(:evidences) { non_conflicting }
 
-      describe 'adds a new evidence' do
-        it 'with "vat_number" as the evidence name'
-        it 'with its country as the evidence value'
+      it 'returns true' do
+        stub_evidences!
+        expect(subject.enough_evidences?).to be true
       end
     end
 
-    # TODO
-    describe 'receiving an Address object' do
-    end
+    context 'without non conflicting evidences' do
+      let(:evidences) { conflicting }
 
-    describe 'receiving other than Hash or VatNumber object' do
-      it 'raises an error' do
-        expect { subject << 'lol' }.to raise_error ArgumentError, /evidence/i
+      it 'returns false' do
+        stub_evidences!
+        expect(subject.enough_evidences?).to be false
       end
     end
   end
 
-  describe '#enough?' do
+  describe '#evidenced_country_code' do
   end
 
-  describe '#with_vat' do
+  # TODO alias
+  describe '#taxables?' do
   end
 
-  describe '#evidences' do
-  end
-  describe '#evidences=' do
+  describe '#validate_on_vies!' do
   end
 
-
-  describe '#consumer' do
-  end
-  describe '#consumer=' do
-  end
-
-  describe '#vat_number' do
-  end
-  describe '#vat_number=' do
+  describe '#valid_vat_numbers?' do
+    context 'VAT number are not validated' do
+      it 'returns nil'
+    end
   end
 
-
-  describe '#e_commerce!' do
+  describe '#should_vat_be_charged?' do
   end
 
   describe '#vat_rates' do
   end
 
-  describe '#conflictives' do
+  describe '#vat_for' do
+  end
+
+  describe '#add_vat_tovat' do
+  end
+
+  describe '#open' do
+  end
+
+  describe '#consumer' do
+  end
+  describe '#supplier' do
   end
   
 end
