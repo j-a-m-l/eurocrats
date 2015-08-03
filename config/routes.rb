78346674bc7419eb147f5235c97be526886fb385
noijd <ju.an.marluc@gmail.com>
2015-08-03 15:40:01 +0100

    Add the VAT number validation route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Eurocrat::Engine.routes.draw do
 
   scope format: 'json' do
-    get 'vat-numbers/:vat_number' => 'vat_numbers#show', as: 'vat_number'
+    get 'vat-numbers/:vat_number' => 'vat_numbers#show', as: 'vat_number_validation'
   end
 
 end
---
+++ spec/routing/vat_numbers_controller_spec.rb
@@ -0,0 +1,20 @@
+describe Eurocrat::VatNumbersController do
+
+  routes { Eurocrat::Engine.routes }
+
+  let(:vat_number) { 'XX12345678' }
+
+  describe 'paths' do
+    it { expect(vat_number_validation_path vat_number).to eq "/vat-numbers/#{vat_number}" }
+  end
+
+  context 'JSON format' do
+    let(:params) { {format: 'json'} }
+
+    it 'to #show' do
+      expect(get vat_number_validation_path vat_number)
+        .to route_to 'eurocrat/vat_numbers#show', params.merge(vat_number: vat_number)
+    end
+  end
+
+end
