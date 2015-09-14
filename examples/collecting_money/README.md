499330b01c1eb1ba483c3ff435f16cdde88121fa
jaml <dev@jaml.site>
2015-09-14 19:47:05 +0100

    Add a simple example of using Context for collecting money

---
+++ examples/collecting_money/README.md
@@ -0,0 +1,10 @@
+This example has 2 files:
+* `config/initializers/eurocrats.rb`
+* `app/controllers/my_simple_controller.rb`
+
+`config/initializers/eurocrats.rb`
+----------------------------------
+Configures the default supplier (the person or company that sells something or provides a service).
+
+`app/controllers/my_simple_controller.rb`
+-----------------------------------------
---
+++ examples/collecting_money/app/controllers/my_simple_controller.rb
@@ -0,0 +1,46 @@
+class MySimpleController < ApplicationController
+
+  def create
+
+    # Adds the VAT if necessary (and determines the country if has not been done)
+    total_amount = eurocrats.with_vat product.price
+
+    @transaction = MyPayment.authorize_and_settle! total_amount
+
+  # Several things could raise an Eurocrats::Error while is determining the
+  # evidenced country:
+  #  * The 2 evidences (IP address and billing address) do not match
+  #  * The VIES validation fails
+  #  * The billing address has a country code that does not exist
+  rescue Eurocrats::Error
+    @epic_fail = true
+  end
+  
+  private
+
+    # This method returns an Eurocrat::Context with the default supplier and a
+    # customer that has a VAT number.
+    #
+    # It includes 2 evidences, one explicit ("billing_address") and other, the
+    # IP address, that has been captured by `request.eurocrats` already
+    def eurocrats
+      customer = Eurocrats::Customer.new
+      customer.vat_number = vat_number_param if vat_number_param
+
+      request.eurocrats Eurocrats.default_supplier, customer
+      request.eurocrats['billing_address'] = billing_address_param
+    end
+
+    def billing_address_param
+      params.require :billing_address, :country_code
+    end
+
+    def vat_number_param
+      params.require :customer, :vat_number
+    end
+
+    def product
+      MyProduct.find params.require(:id)
+    end
+
+end
