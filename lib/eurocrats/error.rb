1acfb6ff081d5e8d8d2ad940a9691865c375137e
jaml <dev@jaml.site>
2015-09-15 09:51:34 +0100

    Some explanation of the exceptions

--- lib/eurocrats/error.rb
+++ lib/eurocrats/error.rb
@@ -3,14 +3,16 @@ module Eurocrats
   class Error < StandardError; end
 
   # Something that requires n evidences matching, has been called without having
-  # that n evidences matching.
+  # those n evidences matching.
   class ConflictingEvidencesError < Error; end
 
+  # Country code does not exist or it does not have a valid ISO 3166-1 format.
   class InvalidCountryCodeError < Error; end
 
+  # Error while trying to perform a VIES request of any type.
   class ViesError < Error; end
 
-  # VAT number syntax is invalid
+  # VAT number syntax is invalid.
   class InvalidVatNumberError < Error; end
 
 end
