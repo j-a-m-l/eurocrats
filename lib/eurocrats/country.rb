93bc6d584b59c23628627b02f472637f667e59bf
jaml <dev@jaml.site>
2015-09-17 18:20:34 +0100

    Add `vat_rates_in` and `in_european_union?` class methods to `Country`, and some aliases, and use `ISO3166::Country` as its parent class, so now has much more functionality.

--- lib/eurocrats/country.rb
+++ lib/eurocrats/country.rb
@@ -1,18 +1,15 @@
 module Eurocrats
 
-  # VAT rates for e-services are the same than the standard: http://ec.europa.eu/taxation_customs/tic/public/vatRates/vatratesSearch.html
+  # VAT rates for e-services are the same than the standard ones:
+  # http://ec.europa.eu/taxation_customs/tic/public/vatRates/vatrates.html
   # TYPES = %[superreduced reduced standard parking]
   # e-commerce => standard 
-  # reduces is array
+  # reduced is an array
 
-  # TODO inherit from Country (countries gem)?
-
-  ##
   # This Country class simplifies the handling of different country codes.
+  # It extends the Country class from the `countries` gem.
   #
-  # It only converts any country code to the ISO 3166-1 alpha-2 equivalent.
-  #
-  class Country
+  class Country < ISO3166::Country
 
     class << self
 
@@ -26,7 +23,7 @@ module Eurocrats
         end
 
         if code.length == 2
-          ISO3166::Country[code] ? code : raise_non_existent(code, 'alpha-2')
+          self[code] ? code : raise_non_existent(code, 'alpha-2')
 
         elsif code.length == 3
 
@@ -38,10 +35,19 @@ module Eurocrats
       end
       alias to_alpha2 code_to_alpha2
 
+      def vat_rates_in country_or_code
+        as_country(country_or_code).vat_rates
+      end
+
+      def in_european_union? country_or_code
+        as_country(country_or_code).in_eu?
+      end
+      alias in_eu? in_european_union?
+
       private
 
         def search_and_convert_code_to_alpha2 code, key, type
-          country = ISO3166::Country.find_all_by key, code
+          country = find_all_by key, code
           if country.any?
             country.values.first['alpha2']
           else
@@ -53,8 +59,22 @@ module Eurocrats
           raise Eurocrats::InvalidCountryCodeError.new "\"#{code}\" is not a ISO 3166-1 #{type} code"
         end
 
+        def as_country country_or_code
+          case country_or_code
+            when self             then country_or_code
+            when ISO3166::Country then self[ country_or_code.alpha2 ]
+            else                       self[ to_alpha2(country_or_code) ]
+          end
+        end
+
     end
 
-  end
+    alias code alpha2
+    alias alpha2_code alpha2
 
+    # TODO
+    # def == other
+    # end
+
+  end
 end
--- spec/lib/eurocrats/country_spec.rb
+++ spec/lib/eurocrats/country_spec.rb
@@ -1,5 +1,7 @@
 describe Eurocrats::Country do
 
+  let(:eu_country) { described_class['DE'] }
+
   # TODO test alias
   describe '.code_to_alpha2' do
     let(:exception) { Eurocrats::InvalidCountryCodeError }
@@ -60,7 +62,70 @@ describe Eurocrats::Country do
         expect { described_class.code_to_alpha2 'example' }.to raise_error exception
       end
     end
+  end
+  describe '.to_alpha2' do
+  end
 
+  describe '.vat_rates_in' do
+    context 'receiving an Eurocrats::Country instance' do
+      it 'returns the VAT rates of the country' do
+        expect(described_class.vat_rates_in eu_country).to eq eu_country.vat_rates
+      end
+    end
+
+    context 'receiving an ISO3166::Country instance' do
+      let(:eu_country) { ISO3166::Country['DE'] }
+
+      it 'returns the VAT rates of the country' do
+        expect(described_class.vat_rates_in eu_country).to eq eu_country.vat_rates
+      end
+    end
+
+    context 'receiving an ISO 3166-2 alpha-2 country code' do
+      it 'returns the VAT rates of the country' do
+        expect(described_class.vat_rates_in eu_country.alpha2).to eq eu_country.vat_rates
+      end
+    end
+
+    # TODO more country codes
+  end
+
+  describe '.in_european_union?' do
+    let(:is_eu_member) { double 'Value of :in_eu?' }
+
+    context 'receiving an Eurocrats::Country instance' do
+      it 'returns if the country belongs to the European Union' do
+        expect(eu_country).to receive(:in_eu?).and_return is_eu_member
+        expect(described_class.in_european_union? eu_country).to eq is_eu_member
+      end
+    end
+
+    context 'receiving an ISO3166::Country instance' do
+      let(:eu_country) { ISO3166::Country['DE'] }
+
+      it 'returns if the country belongs to the European Union' do
+        country_double = double in_eu?: is_eu_member
+        expect(described_class).to receive(:[]).with(eu_country.alpha2).and_return country_double
+        expect(described_class.in_european_union? eu_country).to eq is_eu_member
+      end
+    end
+
+    context 'receiving an ISO 3166-2 alpha-2 country code' do
+      it 'returns if the country belongs to the European Union' do
+        country_double = double in_eu?: is_eu_member
+        expect(described_class).to receive(:[]).with(eu_country.alpha2).and_return(country_double).at_least 1
+        expect(described_class.in_european_union? eu_country.alpha2).to eq is_eu_member
+      end
+    end
+
+    # TODO more country codes
+  end
+  describe '.in_eu?' do
+  end
+
+  describe '#code' do
+  end
+  describe '#alpha2_code' do
   end
 
 end
